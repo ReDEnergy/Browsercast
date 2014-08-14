cc99c4a7e24a75422c53d8c8e78f019850ede3ba
Gabriel Ivanica <ivanicagabriel@gmail.com>
2014-08-14 16:43:34 +0300

    added download options

---
+++ public/js/app/config.js
@@ -0,0 +1,17 @@
+define(function(require, exports, module) {
+	'use strict';
+
+	// Load Modules
+	// var Module = require('path/to/module');
+
+	// API
+
+	var Presentation = {
+		title: 'Browsercast',
+		author: 'Browsercast',
+		description: ''
+	};
+
+	// PUBLIC API
+	exports.Presentation = Presentation;
+});
